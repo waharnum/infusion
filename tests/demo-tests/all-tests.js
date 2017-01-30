47e82b8e2fa66ed62a56ca3fca81a106c1e47c60
Avtar Gill <avtargill@gmail.com>
2017-01-29 21:53:11 -0500

    Adds example that closes overview panel

--- tests/demo-tests/all-tests.js
+++ tests/demo-tests/all-tests.js
@@ -1,6 +1,6 @@
 /*
 
-    Test the "get" function with an external URL.
+    Load the keyboard-a11y demo and confirm the overview panel closes properly.
 
  */
 /* eslint-env node */
@@ -11,10 +11,14 @@ var gpii = fluid.registerNamespace("gpii");
 require("gpii-webdriver");
 gpii.webdriver.loadTestingSupport();
 
-fluid.defaults("infusion.demos.webdriver.caseHolder", {
+var isOverviewPanelVisible = function () {
+    return $(".fl-overviewPanel-closeControl").attr("aria-expanded");
+};
+
+fluid.defaults("demos.overviewPanel.caseHolder", {
     gradeNames: ["gpii.test.webdriver.caseHolder"],
     rawModules: [{
-        name: "Testing a remote Infusion demo...",
+        name: "Testing that the overview panel closes...",
         tests: [
             {
                 name: "Access the keyboard-a11y demo...",
@@ -26,27 +30,37 @@ fluid.defaults("infusion.demos.webdriver.caseHolder", {
                     },
                     {
                         event:    "{testEnvironment}.webdriver.events.onGetComplete",
-                        listener: "{testEnvironment}.webdriver.getPageSource",
-                        args:     []
+                        listener: "{testEnvironment}.webdriver.findElement",
+                        args:     [gpii.webdriver.By.css(".fl-overviewPanel-closeControl")]
                     },
                     {
-                        event:    "{testEnvironment}.webdriver.events.onGetPageSourceComplete",
-                        listener: "jqUnit.assertNotUndefined",
-                        args:     ["There should be page content...", "{arguments}.0"]
-                    }                  
+                        event:    "{testEnvironment}.webdriver.events.onFindElementComplete",
+                        listener: "{testEnvironment}.webdriver.actionsHelper",
+                        args:     [{fn: "click", args: ["{arguments}.0"]}]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onActionsHelperComplete",
+                        listener: "{testEnvironment}.webdriver.executeScript",
+                        args:     [isOverviewPanelVisible]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onExecuteScriptComplete",
+                        listener: "jqUnit.assertEquals",
+                        args:     ["The overview panel is visible...", "{arguments}.0", "false"]
+                    }
                 ]
             }
         ]
     }]
 });
 
-fluid.defaults("infusion.demos.webdriver.environment", {
+fluid.defaults("demos.webdriver.environment", {
     gradeNames: ["gpii.test.webdriver.testEnvironment"],
     components: {
         caseHolder: {
-            type: "infusion.demos.webdriver.caseHolder"
+            type: "demos.overviewPanel.caseHolder"
         }
     }
 });
 
-gpii.test.webdriver.allBrowsers({ baseTestEnvironment: "infusion.demos.webdriver.environment" });
+gpii.test.webdriver.allBrowsers({ baseTestEnvironment: "demos.webdriver.environment" });
